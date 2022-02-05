0b16010c2fb5f5dadd026dda766ba3a362d1a08d
techcto <smoore@solodev.com>
2022-02-05 14:41:06 -0500

    add shared resources

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,12 @@
 DEPLOY=1
 DATE=$(date +%d%H%M)
 
+# aws s3 cp shared.yaml s3://solodev-kubernetes/cloudformation/shared.yaml --acl public-read
+
+# echo "Create Shared Resources"
+# aws cloudformation create-stack --disable-rollback --stack-name tmp-shared-${DATE} --disable-rollback --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
+#     --template-url https://s3.amazonaws.com/solodev-kubernetes/cloudformation/shared.yaml
+
 aws s3 cp eks.yaml s3://solodev-kubernetes/cloudformation/eks.yaml --acl public-read
 aws s3 cp nodegroup.yaml s3://solodev-kubernetes/cloudformation/nodegroup.yaml --acl public-read
 aws s3 cp solodev-eks-nginx-ingress.template.yaml s3://solodev-kubernetes/cloudformation/solodev-eks-nginx-ingress.template.yaml --acl public-read
@@ -9,4 +15,14 @@ aws s3 cp solodev-eks-external-dns.template.yaml s3://solodev-kubernetes/cloudfo
 aws s3 cp solodev-eks-lets-encrypt.template.yaml s3://solodev-kubernetes/cloudformation/solodev-eks-lets-encrypt.template.yaml --acl public-read
 aws s3 sync functions/packages s3://solodev-kubernetes/cloudformation/functions/packages --delete
 
-./bin/stacks.sh
+echo "Create AWS EKS Mega Cluster"
+aws cloudformation create-stack --disable-rollback --stack-name tmp-kube-${DATE} --disable-rollback --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
+    --parameters file://bin/eks.json \
+    --template-url https://s3.amazonaws.com/solodev-kubernetes/cloudformation/eks.yaml
+
+# aws s3 cp webstack.yaml s3://solodev-kubernetes/cloudformation/webstack.yaml --acl public-read
+
+# echo "Create Webstack"
+# aws cloudformation create-stack --disable-rollback --stack-name tmp-webstack-${DATE} --disable-rollback --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
+#     --parameters file://bin/webstack.json \
+#     --template-url https://s3.amazonaws.com/solodev-kubernetes/cloudformation/webstack.yaml
